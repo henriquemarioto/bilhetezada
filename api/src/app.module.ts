fc47a1204ed64fb29f6d0b6baa7c6974915d5b48
Marioto <henrique.marioto@outlook.com>
2024-01-30 22:31:45 -0300

    feat: :sparkles: implement cache manager and save session on redis

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,13 +6,25 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
     CustomerModule,
     MongooseModule.forRoot(process.env.MONGODB_URL),
     AuthModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      url: process.env.REDIS_URL,
+      ttl: 86400,
+    }),
     PassportModule.register({ session: true }),
   ],
   controllers: [AppController],
--- api/src/main.ts
+++ api/src/main.ts
@@ -3,11 +3,26 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const redisClient = createClient({
+    url: process.env.REDIS_URL,
+  });
+  redisClient.connect();
+  redisClient.on('connect', () =>
+    console.log('Connected to redis successfully.'),
+  );
+  redisClient.on('error', (err) =>
+    console.log('Unable to connect to redis. ' + err),
+  );
+
   app.use(
     session({
+      store: new RedisStore({ client: redisClient }),
       name: 'Bilhetezada',
       secret: process.env.SESSION_SECRET,
       resave: Boolean(process.env.SESSION_RESAVE),
@@ -19,7 +34,9 @@ async function bootstrap() {
   );
   app.use(passport.initialize());
   app.use(passport.session());
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
