f99c868bf0180ff89c4d26ae38effbd91431d117
marioto <henrique.marioto@outlook.com>
2025-01-30 13:18:14 -0300

    feat: TimezoneService unit tests

---
+++ api/src/modules/shared/services/timezone.service.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import TimezoneService from './timezone.service';
+import { Timezones } from '../enums/timezones.enum';
+
+describe('TimezoneService', () => {
+  let timezoneService: TimezoneService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TimezoneService],
+    }).compile();
+
+    timezoneService = module.get<TimezoneService>(TimezoneService);
+  });
+
+  it('should be defined', () => {
+    expect(timezoneService).toBeDefined();
+  });
+
+  describe('getAllTimezones', () => {
+    it('should return all timezones', () => {
+      expect(timezoneService.getAllTimezones()).toEqual(
+        Object.values(Timezones),
+      );
+    });
+  });
+
+  describe('isValidTimezone', () => {
+    it('should validate if timezone exists', () => {
+      expect(
+        timezoneService.isValidTimezone(Timezones.AMERICA_SAO_PAULO),
+      ).toEqual(true);
+    });
+
+    it('should return false if timezone not exists', () => {
+      expect(timezoneService.isValidTimezone('invalid')).toEqual(false);
+    });
+  });
+});
