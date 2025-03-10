768576c4f8271b3497a900dcb6d3ef5a6f54738e
marioto <henrique.marioto@outlook.com>
2025-03-10 18:16:40 -0300

    wip: login screen test

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,28 +1,68 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <article>
-      <div>
+    <article className="w-lvw h-lvh flex items-center justify-center bg-foreground">
+      <div className="flex flex-col gap-6 text-secondary border-2 border-primary p-6 rounded-2xl">
+        <p className="text-2xl font-bold">Bilhetezada</p>
         <section>
-          <p>Bilhetezada</p>
-        </section>
-        <section>
-          <h1>Login</h1>
+          <h1 className="text-xl">Login</h1>
           <h2>Gerencie seus eventos com Bilhetezada</h2>
         </section>
-        <form>
-          <div>
-            <label>Email</label>
-            <input type="email" name="email" />
-          </div>
-          <div>
-            <label>Senha</label>
-            <input type="password" name="password" />
-          </div>
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email@email.com" required {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Digite seu melhor email.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" variant="secondary">Entrar</Button>
+          </form>
+        </Form>
         <section>
-          <Button>Entrar</Button>
         </section>
       </div>
     </article>
